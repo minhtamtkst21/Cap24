1af69756f31e73c458c0684e6402900ab6e1facf
phuongnguyen2505 <phuong.t13579@gmail.com>
2021-12-14 20:25:40 +0700

    Index student list

---
+++ WebApplication/Areas/Faculty/Controllers/DanhSachSinhVienController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Areas.Faculty.Controllers
+{
+    public class DanhSachSinhVienController : Controller
+    {
+        // GET: Faculty/DanhSachSinhVien
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
